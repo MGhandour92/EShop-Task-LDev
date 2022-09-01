92cd23b680103f8c09439834cecec793c731f1e8
Mohamed El Ghandour <m.el_ghandour@hotmail.com>
2022-09-01 13:11:18 +0200

    init

---
+++ Electronics_Shop_Sample/Electronics_Shop_Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Electronics_Shop_Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
